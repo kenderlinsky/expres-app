1b91c96896865ef6a9f4b253ea7d9f8784bc06bd
kenderlinsky <kenderlinsky@gmail.com>
2025-12-02 21:46:59 +0300

    edit index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h4>Hello World!</h4>')
 })
 
 const PORT = 8080
