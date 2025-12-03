d1e602cbbe1135a042e9f316cc5b577dfead4087
kenderlinsky <kenderlinsky@gmail.com>
2025-12-03 10:41:04 +0300

    add: build from builder.sh

--- builder.sh
+++ builder.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
+#!/usr/bin/env sh
+set -euo pipefail
 IFS=$'\n\t'
 
-#CONFIG
-
 SCRIPT_NAME="$(basename "$0")"
-
-#LOGGING
 
 log() {
     # log "message"
@@ -16,49 +12,22 @@ log() {
         "$*" >&2
 }
 
-debug() {
-    if [[ "${VERBOSE:-0}" -ne 0 ]]; then
-        log "[DEBUG] $*"
-    fi
-}
-
-# CLEANUP & ERROR HANDLING
-
 TMPDIR="$(mktemp -d)"
-debug "Using temp dir: $TMPDIR"
 
 cleanup() {
-    debug "Running cleanup..."
     [[ -d "$TMPDIR" ]] && rm -rf "$TMPDIR" || true
 }
 
-on_error() {
-    local code=$?
-    local line="${BASH_LINENO[0]}"
-    log "ERROR: Script failed at line $line with exit code $code"
-}
-
-on_term() {
-    log "Received termination signal (SIGTERM/SIGINT/SIGHUP)"
-    cleanup
-    exit 0
-}
-
-trap on_error ERR
-trap cleanup EXIT
-trap on_term SIGTERM SIGINT SIGHUP
-
 if [ $# -lt 2 ]; then
-    echo "use two arguments: github repository, dockerhub repository" >&2
+    echo "Usage: $0 <github-repo> <docker-image:tag>" >&2
     exit 1
 fi
 
-# MAIN FUNCTIONALITY
 main() {
 GH_PATH="$1"
 REPO_URL="https://github.com/${GH_PATH}.git"
 REPO_DIR="$(basename "$GH_PATH")"
-
+DOCKER_TAG="$2"
 log "Repo path: $GH_PATH"
 log "Clone URL: $REPO_URL"
 log "Target dir: $REPO_DIR"
@@ -67,11 +36,11 @@ cd "$TMPDIR"
 git clone "$REPO_URL"
 log "Entering directory: $REPO_DIR"
 cd "$REPO_DIR"
-echo "Now inside: $(pwd)"
-log "docker build"
-docker build . -t "$2"
-docker tag "$2" "$2"
-docker push "$2"                                                                                                                  
+log "build docker image $DOCKER_TAG"
+cat /docker/password.txt | docker login registry-1.docker.io -u kenderlinsky --password-stdin
+docker build -f Dockerfile.build -t "$DOCKER_TAG" . 
 }
 
 main "$@"
+
+cleanup
